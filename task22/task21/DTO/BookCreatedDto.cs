c807528674ee99702619cbbfb1c2a8dfbc97593b
Gihadelrwiny <gihadelrwiny2004@gmail.com>
2026-06-22 19:02:24 +0300

    first push

---
+++ task22/task21/DTO/BookCreatedDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace task21.DTO
+{
+    public class BookCreatedDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [MaxLength(100)]
+        public string Author{ get; set; }
+    }
+}
